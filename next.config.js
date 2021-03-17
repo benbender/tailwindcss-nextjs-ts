9060fc55c7b13a562a62a70ebae0ebcba8144419
Benjamin Bender <bb@codepoet.de>
2021-03-17 22:51:24 +0100

    Test preact

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
-const config = {
+const withPreact = require("next-plugin-preact");
+
+const config = withPreact({
   poweredByHeader: false,
   reactStrictMode: true,
-  experimental: { optimizeCss: true },
-};
+  experimental: {
+    optimizeCss: true,
+  },
+});
 
 module.exports = config;
